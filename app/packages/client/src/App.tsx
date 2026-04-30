ff21f9f1414ae9e73b3cb3b824f484bcc8b19696
jzkime <jazdkime@gmail.com>
2026-04-30 12:32:19 -0700

    run biome on existing code

--- app/packages/client/src/App.tsx
+++ app/packages/client/src/App.tsx
@@ -1,14 +1,12 @@
-import { WYSIWYG } from "./wysiwyg/WYSIWYG"
+import { WYSIWYG } from "./wysiwyg/WYSIWYG";
 
 function App() {
-  return (
-    <div>
-      <div style={{paddingBottom: 16}}>
-        App
-      </div>
-      <WYSIWYG />  
-    </div>
-  )
+    return (
+        <div>
+            <div style={{ paddingBottom: 16 }}>App</div>
+            <WYSIWYG />
+        </div>
+    );
 }
 
-export default App
+export default App;
--- app/packages/client/src/wysiwyg/WYSIWYG.tsx
+++ app/packages/client/src/wysiwyg/WYSIWYG.tsx
@@ -1,24 +1,23 @@
-import { useState } from "react"
-import Editor from "react-simple-wysiwyg"
-import sanitizeHtml from "sanitize-html"
+import { useState } from "react";
+import Editor from "react-simple-wysiwyg";
+import sanitizeHtml from "sanitize-html";
 
-const configSanitizeHtml = (input: string) => sanitizeHtml(input)
+const configSanitizeHtml = (input: string) => sanitizeHtml(input);
 
 export const WYSIWYG = () => {
-    const [html, setHtml] = useState("")
-    const [showParsedHtml, setShowParsedHtml] = useState(false)
-    const [parsedHtml, setParsedHtml] = useState("")
+    const [html, setHtml] = useState("");
+    // const [showParsedHtml, setShowParsedHtml] = useState(false);
+    // const [parsedHtml, setParsedHtml] = useState("");
 
     const handleChange = (input: string) => {
-        console.log(input)
-        setHtml(configSanitizeHtml(input))
-    }
+        setHtml(configSanitizeHtml(input));
+    };
 
     return (
         <Editor
-            containerProps={{ style: {whiteSpace: "pre-line" }}}
-            value={html} 
+            containerProps={{ style: { whiteSpace: "pre-line" } }}
+            value={html}
             onChange={(e) => handleChange(e.target.value)}
         />
-    )
-}
+    );
+};
